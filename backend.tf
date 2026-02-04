1aa0250c0892831aca1312a7b6d53f1fbb3574da
brahmaji.gopalam <brahma.scm9@gmail.com>
2026-02-04 15:12:33 +0000

    added fix for missiing quotes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hackathon-terraform-state-bucket
+    bucket         = "hackathon-terraform-state-bucket"
     key            = "terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-locks"
